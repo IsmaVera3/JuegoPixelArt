43d0c75fac210293e2d803fedd97189c6350835a
IsmaVera3 <132963276+IsmaVera3@users.noreply.github.com>
2023-08-08 10:13:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//import Swal from "sweetalert2/dist/sweetalert2.js";
+//import "sweetalert2/src/sweetalert2.scss";
+
 let celeste = 0;
 let marron = 1;
 let marronClaro = 2;
@@ -6,28 +9,188 @@ let rojo = 4;
 let verde = 5;
 
 let hamburguesaHecha = [
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 1, 1 ,2 ,1 ,1 ,2 ,0 ,0],
-    [0, 1, 2, 1 ,1 ,1 ,1 ,1 ,1 ,0],
-    [0, 2, 1, 1 ,2 ,1 ,2 ,1 ,2 ,0],
-    [0, 5, 5, 1 ,1 ,1 ,1 ,5 ,5 ,0],
-    [0, 3, 4, 5 ,5 ,5 ,5 ,4 ,3 ,0],
-    [0, 3, 3, 4 ,4 ,4 ,4 ,3 ,3 ,0],
-    [0, 1, 3, 3 ,3 ,3 ,3 ,3 ,1 ,0],
-    [0, 0, 1, 1 ,1 ,1 ,1 ,1 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0]
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(212, 172, 100)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(212, 172, 100)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(180, 124, 36)",
+    "rgb(212, 172, 100)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(212, 172, 100)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(212, 172, 100)",
+    "rgb(180, 124, 36)",
+    "rgb(212, 172, 100)",
+    "rgb(180, 124, 36)",
+    "rgb(212, 172, 100)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(28, 172, 28)",
+    "rgb(28, 172, 28)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(28, 172, 28)",
+    "rgb(28, 172, 28)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(68, 44, 4)",
+    "rgb(252, 4, 4)",
+    "rgb(28, 172, 28)",
+    "rgb(28, 172, 28)",
+    "rgb(28, 172, 28)",
+    "rgb(28, 172, 28)",
+    "rgb(252, 4, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(68, 44, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(252, 4, 4)",
+    "rgb(252, 4, 4)",
+    "rgb(252, 4, 4)",
+    "rgb(252, 4, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(180, 124, 36)",
+    "rgb(68, 44, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(68, 44, 4)",
+    "rgb(180, 124, 36)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(180, 124, 36)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
+    "rgb(52, 172, 236)",
 ];
-let hamburguesaVacia = [
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0, 0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0],
-    [0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0]
+let hamburguesaVacia = [];
+
+// Obtén el contenedor de la grilla y la paleta
+const gridContainer = document.getElementById("grid-container");
+const palette = document.getElementById("palette");
+const gridColors = [];
+
+// Definimos una paleta de colores
+const colors = [
+    "#b47c24",
+    "#1cac1c",
+    "#442c04",
+    "#d4ac64",
+    "#fc0404",
+    "#34acec",
 ];
 
+// Crea la grilla
+function createGrid() {
+    for (let i = 0; i < 100; i++) {
+        // Cambia "100" por el número de celdas que deseas en tu grilla
+        const cell = document.createElement("div");
+        cell.classList.add("grid-item");
+        gridContainer.appendChild(cell);
+
+        gridColors.push(""); // Agrega un valor inicial vacío al arreglo
+    }
+}
+
+// Función para cambiar el color de la celda cuando el usuario haga clic en ella
+function changeColor(event) {
+    if (event.target.classList.contains("grid-item")) {
+        const selectedColor = palette.querySelector(".selected");
+        if (selectedColor) {
+            const color = selectedColor.style.backgroundColor;
+            event.target.style.backgroundColor = color;
+
+            // Obtiene el índice de la celda en el arreglo gridColors
+            const cellIndex = Array.from(gridContainer.children).indexOf(
+                event.target
+            );
+            gridColors[cellIndex] = color; // Actualiza el color en el arreglo
+            //console.log(gridColors);
+        }
+    }
+    if (sonIguales(hamburguesaHecha, gridColors)) {
+        Swal.fire("Bien Hecho!", "Has completado el juego!", "success");
+    } else {
+        console.log("se pregunto");
+    }
+}
+
+// Función para seleccionar el color de la paleta
+function selectColor(event) {
+    if (event.target.classList.contains("color")) {
+        const selectedColor = palette.querySelector(".selected");
+        if (selectedColor) {
+            selectedColor.classList.remove("selected");
+        }
+        event.target.classList.add("selected");
+    }
+}
+
+// Agrega un evento de clic a la grilla
+gridContainer.addEventListener("click", changeColor);
+
+// Agrega un evento de clic a la paleta
+palette.addEventListener("click", selectColor);
+
+// Inicializa la grilla
+createGrid();
+console.log(gridColors.length);
+
+function sonIguales(hamburguesaHecha, gridColors) {
+    for (let i = 0; i < hamburguesaHecha.length; i++) {
+        //console.log("itaracion for numero:" + i);
+        if (hamburguesaHecha[i] !== gridColors[i]) {
+            //console.log("itaracion if numero:" + i);
+            return false;
+        }
+    }
+    return true;
+}
 
